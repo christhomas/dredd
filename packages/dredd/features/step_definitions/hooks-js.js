0a78e427d959ed86807a47be373b9d25ccf67d30
Chris Thomas <chris.thomas@antimatter-studios.com>
2026-08-15 15:09:59 +0200

    test: move the end-to-end suite to @cucumber/cucumber
    - Replace cucumber 6.0.5, which was renamed to @cucumber/cucumber in 2020 and cannot load ES module support code.
    - Convert the step definitions and the world to ES modules, since they sit in a package that now declares itself one.
    - Escape the slashes in the step expressions, which a Cucumber Expression reads as the separator of an alternative.

--- packages/dredd/features/step_definitions/hooks-js.js
+++ packages/dredd/features/step_definitions/hooks-js.js
@@ -1,17 +1,17 @@
-const { spawn } = require('child_process');
-const { expect } = require('chai');
-const { Given, When, Then } = require('cucumber');
+import { spawn } from 'child_process';
+import { expect } from 'chai';
+import { Given, When, Then } from '@cucumber/cucumber';
 
 
-Given('I have an API description with transactions "GET /articles" and "POST /articles"', function () {
+Given('I have an API description with transactions "GET \\/articles" and "POST \\/articles"', function () {
   this.dredd.apiDescription = `test/fixtures/blog/apidesc${this.apiDescriptionExt}`;
 });
 
-Given('I have an implementation, which requires auth on "POST /articles"', function () {
+Given('I have an implementation, which requires auth on "POST \\/articles"', function () {
   this.dredd.args.push('--server=node test/fixtures/blog/app.js');
 });
 
-Given('I have hooks adding auth to "POST /articles"', function () {
+Given('I have hooks adding auth to "POST \\/articles"', function () {
   this.dredd.args.push(`--hookfiles=test/fixtures/blog/hooks${this.hooksExt}`);
 });
 
@@ -29,14 +29,14 @@ When('I run Dredd', { timeout: 10 * 1000 }, function (callback) {
   cli.on('exit', (exitStatus) => { this.dredd.exitStatus = exitStatus; callback(); });
 });
 
-Then('the "GET /articles" test passes', function () {
+Then('the "GET \\/articles" test passes', function () {
   expect(this.dredd.output).to.contain('pass: GET (200) /articles');
 });
 
-Then('the "POST /articles" test passes', function () {
+Then('the "POST \\/articles" test passes', function () {
   expect(this.dredd.output).to.contain('pass: POST (201) /articles');
 });
 
-Then('the "POST /articles" test fails', function () {
+Then('the "POST \\/articles" test fails', function () {
   expect(this.dredd.output).to.contain('fail: POST (201) /articles');
 });
--- packages/dredd/features/support/world.js
+++ packages/dredd/features/support/world.js
@@ -1,4 +1,4 @@
-const { setWorldConstructor } = require('cucumber');
+import { setWorldConstructor } from '@cucumber/cucumber';
 
 
 const API_DESCRIPTION_EXTS = {
